acdba16516c85cf2420887d5630c6d1ba7a78a34
SnowReplicator <snowrepl@gmail.com>
2022-03-17 08:33:16 +0300

    carcas

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ru.zorro.static_select
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- start ---");
+            Func1();
+            Console.WriteLine("--- stop ---");
+        }
+
+        private static void Func1()
+        {
+
+        }
+    }
+}
